20c5284c93f42c29c9ad24877c28d3a5419ec99b
Daniel Hoang <thesubway@gmail.com>
2016-01-27 15:39:06 -0800

    Allow default title, remove custom title for home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#Returns title with additional pipe (vertical bar)
+	def full_title(page_title = '')
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			page_title += " | " + base_title
+		end
+	end
 end
