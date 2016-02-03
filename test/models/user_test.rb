2bf6174b6c44d76ba159bc657a6d93c9b69431c4
Daniel Hoang <thesubway@gmail.com>
2016-02-02 22:16:42 -0800

    Add test case for valid user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+  	@user = User.new(name: "Dan", email: "dan@example.com")
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
 end
